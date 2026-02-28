6ba0baa8929d3b1ddc70e06a47f6948b55c7b518
ranwang <ranwang.osbert@outlook.com>
2026-02-28 12:58:20 +0800

    add l3build file build.lua

---
+++ build.lua
@@ -0,0 +1,108 @@
+--[==========================================[--
+   L3BUILD FILE FOR ELEGANTNOTE
+     Check PDF File & Directory After Build
+--]==========================================]--
+
+--[==========================================[--
+                Basic Information
+             Do Check Before Upload
+--]==========================================]--
+module           = "elegantnote"
+version          = "2.60"
+maintainer       = "Ran Wang"
+uploader         = maintainer
+maintainid       = "ElegantLaTeX"
+email            = "ranwang.osbert@outlook.com"
+repository       = "https://github.com/" .. maintainid .. "/" .. module
+announcement     = ""
+note             = ""
+summary          = "Elegant LaTeX Template for Notes"
+description      = [[
+ElegantNote is designed for writing working papers, especially for economics
+students. This template is based on the standard LaTeX article class. The
+goal of this template is to make the writing process easier and more
+comfortable.
+]]
+
+--[==========================================[--
+         Build, Pack and Upload To CTAN
+         Do not Modify Unless Necessary
+--]==========================================]--
+ctanzip          = module
+excludefiles     = {"*~"}
+textfiles        = {"*.md", "LICENSE", "*.lua", "*.cls"}
+typesetfiles     = {module .. "-cn.tex", module .. "-en.tex"}
+typesetsuppfiles = {"*.cls"}
+imagesuppdir     = "image"
+
+uploadconfig = {
+  pkg          = module,
+  version      = version,
+  author       = maintainer,
+  uploader     = uploader,
+  email        = email,
+  summary      = summary,
+  description  = description,
+  announcement = announcement,
+  note         = note,
+  license      = "lppl1.3c",
+  ctanPath     = "/macros/latex/contrib/" .. module .. "/",
+  home         = repository,
+  support      = repository .. "/issues",
+  bugtracker   = repository .. "/issues",
+  repository   = repository,
+  development  = "https://github.com/" .. maintainid,
+  update       = true
+}
+
+-- cn uses XeLaTeX, en uses pdfLaTeX
+function typeset(file, dir, exe)
+  dir = dir or typesetdir
+  local cmd
+  if string.match(file, "%-cn%.tex$") then
+    cmd = "latexmk -pdfxe -synctex=1 -interaction=nonstopmode "
+  else
+    cmd = "latexmk -pdf -synctex=1 -interaction=nonstopmode "
+  end
+  return run(dir, cmd .. file)
+end
+
+-- Copy cls and image subdirectory into the typeset build dir
+function docinit_hook()
+  -- Copy .cls
+  for _, glob in pairs(typesetsuppfiles) do
+    cp(glob, currentdir, typesetdir)
+  end
+  -- Copy image/
+  local dest = typesetdir .. "/" .. imagesuppdir
+  mkdir(dest)
+  cp("*", imagesuppdir, dest)
+  -- Copy .tex
+  for _, texfile in pairs(typesetfiles) do
+    cp(texfile, currentdir, typesetdir)
+  end
+  return 0
+end
+
+-- Pack CTAN directory: cls, tex sources, PDFs, and image subdir
+function copyctan()
+  local pkgdir = ctandir .. "/" .. ctanpkg
+  mkdir(pkgdir)
+  -- Copy .cls
+  for _, glob in pairs(typesetsuppfiles) do
+    cp(glob, currentdir, pkgdir)
+  end
+  -- Copy .tex
+  for _, texfile in pairs(typesetfiles) do
+    cp(texfile, currentdir, pkgdir)
+  end
+  -- Copy compiled PDF
+  for _, glob in pairs(pdffiles or {"*.pdf"}) do
+    cp(glob, typesetdir, pkgdir)
+  end
+  -- Copy image/
+  local dest = pkgdir .. "/" .. imagesuppdir
+  mkdir(dest)
+  cp("*", imagesuppdir, dest)
+end
+
